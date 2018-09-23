2a198f1aa5c08a4267c022dfe69dcaecd2b71b95
Nicholas Crofts <nicholas.crofts@gmail.com>
2018-09-23 11:05:40 -0400

    added more seeding for novel loops as well as links to the home page

--- db/seeds.js
+++ db/seeds.js
@@ -18,7 +18,14 @@ const firstNovel = new Novel({
 
 const secondNovel = new Novel({
     writer: "Dashiell Hammet",
-    title: "Red Harvest",
+    title: "Red Harvest", 
+    synopsis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+})
+
+const thirdNovel = new Novel({
+    writer: "Dashiell Hammet",
+    title: "The Maltese Falcon", 
     synopsis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
 
 })
@@ -37,7 +44,7 @@ const firstAuthor = new Author({
 const secondAuthor = new Author({
     name: "Dashiell Hammet",
     about: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    novels: [secondNovel] 
+    novels: [secondNovel, thirdNovel] 
 })
 
 
