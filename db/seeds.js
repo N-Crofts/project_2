0c7ba10eac5691d3f8e36c9001e62ed3ed3bdd83
Nicholas Crofts <nicholas.crofts@gmail.com>
2018-09-25 09:54:41 -0400

    added more seeds with non-filler text

--- db/seeds.js
+++ db/seeds.js
@@ -16,13 +16,13 @@ const firstNovel = new Novel({
 })
 
 const secondNovel = new Novel({
-    writer: "Dashiell Hammet",
+    writer: "Dashiell Hammett",
     title: "Red Harvest", 
     synopsis: "Detective-story master Dashiell Hammett gives us yet another unforgettable read in Red Harvest: When the last honest citizen of Poisonville was murdered, the Continental Op stayed on to punish the guilty--even if that meant taking on an entire town. Red Harvest is more than a superb crime novel: it is a classic exploration of corruption and violence in the American grain."
 })
 
 const thirdNovel = new Novel({
-    writer: "Dashiell Hammet",
+    writer: "Dashiell Hammett",
     title: "The Maltese Falcon", 
     synopsis: "The Maltese Falcon is a 1930 detective novel by Dashiell Hammett, originally serialized in the magazine Black Mask beginning with the September 1929 issue. The story has been adapted several times for the cinema. The main character, Sam Spade, appears in this novel and in three lesser-known short stories and is widely cited as the crystallizing figure in the development of the hard-boiled private detective genre. Raymond Chandler's character Philip Marlowe, for instance, was strongly influenced by Hammett's Spade. Spade was a departure from Hammett's nameless detective, The Continental Op. Spade combined several features of previous detectives, notably his cold detachment, keen eye for detail, unflinching, sometimes ruthless, determination to achieve his own form of justice, and a complete lack of sentimentality."
 })
@@ -31,6 +31,30 @@ const fourthNovel = new Novel({
     writer: "Akimitsu Takagi",
     title: "The Tattoo Murder Case", 
     synopsis: "Kinue Nomura survived World War II only to be murdered in Tokyo, her severed limbs discovered in a room locked from the inside. Gone is the part of her that bore one of the most beautiful full-body tattoos ever rendered. Kenzo Matsushita, a young doctor who was first to discover the crime scene, feels compelled to assist his detective brother, who is in charge of the case. But Kenzo has a secret: he was Kinue’s lover, and soon his involvement in the investigation becomes as twisted and complex as the writhing snakes that once adorned Kinue’s torso.",
+})
+
+const fifthNovel = new Novel({
+    writer: "James M. Cain",
+    title: "The Postman Always Rings Twice", 
+    synopsis: "An amoral young tramp.  A beautiful, sullen woman with an inconvenient husband.  A problem that has only one grisly solution--a solution that only creates other problems that no one can ever solve. First published in 1934 and banned in Boston for its explosive mixture of violence and eroticism, The Postman Always Rings Twice is a classic of the roman noir. It established James M. Cain as a major novelist with an unsparing vision of America's bleak underside, and was acknowledged by Albert Camus as the model for The Stranger."
+})
+
+const sixthNovel = new Novel({
+    writer: "Walter Mosley",
+    title: "Devil in a Blue Dress", 
+    synopsis: "Set in the late 1940s, in the African-American community of Watts, Los Angeles, Devil in a Blue Dress follows Easy Rawlins, a black war veteran just fired from his job at a defense plant. Easy is drinking in a friend's bar, wondering how he'll meet his mortgage, when a white man in a linen suit walks in, offering good money if Easy will simply locate Miss Daphne Monet, a blonde beauty known to frequent black jazz clubs."
+})
+
+const seventhNovel = new Novel({
+    writer: "Dashiell Hammett",
+    title: "The Dain Curse", 
+    synopsis: "Dashiell Hammett virtually invented the hardboiled crime novel, and his creation the Continental Op is a short, squat, and utterly unsentimental tank of a private detective. Miss Gabrielle Dain Leggett is young, wealthy, and a devotee of morphine and religious cults. She has an unfortunate effect on the people around her: they have a habit of dying violently. Is Gabrielle the victim of a family curse? Or is the truth about her weirder and infinitely more dangerous? The Dain Curse is one of the Continental Op's most bizarre cases, and a tautly crafted masterpiece of suspense."
+})
+
+const eighthNovel = new Novel({
+    writer: "James Ellroy",
+    title: "The Black Dahlia", 
+    synopsis: "The Black Dahlia is a roman noir on an epic scale: a classic period piece that provides a startling conclusion to America's most infamous unsolved murder mystery--the murder of the beautiful young woman known as The Black Dahlia."
 })
 
 
@@ -45,9 +69,9 @@ const firstAuthor = new Author({
 })
 
 const secondAuthor = new Author({
-    name: "Dashiell Hammet",
+    name: "Dashiell Hammett",
     about: "Samuel Dashiell Hammett (May 27, 1894 – January 10, 1961) was an American author of hard-boiled detective novels and short stories. He was also a screenwriter and political activist. Among the enduring characters he created are Sam Spade (The Maltese Falcon), Nick and Nora Charles (The Thin Man), and the Continental Op (Red Harvest and The Dain Curse).",
-    novels: [secondNovel, thirdNovel] 
+    novels: [secondNovel, thirdNovel, seventhNovel] 
 })
 
 const thirdAuthor = new Author({
@@ -56,25 +80,58 @@ const thirdAuthor = new Author({
     novels: [fourthNovel] 
 })
 
+const fourthAuthor = new Author({
+    name: "James M. Cain",
+    about: "James Mallahan Cain (July 1, 1892 – October 27, 1977) was an American author and journalist. Cain vehemently opposed labeling, but he is usually associated with the hardboiled school of American crime fiction and is seen as one of the creators of the roman noir. Several of his crime novels inspired successful movies.",
+    novels: [fifthNovel] 
+})
+
+const fifthAuthor = new Author({
+    name: "Walter Mosley",
+    about: "Walter Ellis Mosley (born January 12, 1952) is an American novelist, most widely recognized for his crime fiction. He has written a series of best-selling historical mysteries featuring the hard-boiled detective Easy Rawlins, a black private investigator and World War II veteran living in the Watts neighborhood of Los Angeles, California; they are perhaps his most popular works.",
+    novels: [sixthNovel] 
+})
+
+const sixthAuthor = new Author({
+    name: "James Ellroy",
+    about: "Lee Earle 'James' Ellroy (born March 4, 1948) is an American crime fiction writer and essayist. In 1981, Ellroy published his first novel, Brown's Requiem, a detective story drawing on his experiences as a caddie.[16] He then published Clandestine and Silent Terror (which was later published under the title Killer on the Road). Ellroy followed these three novels with the Lloyd Hopkins Trilogy. The novels are centered on Hopkins, a brilliant but disturbed LAPD robbery-homicide detective, and are set mainly in the 1980s.",
+    novels: [eighthNovel] 
+})
 
 
 ///////////////////////////////////////////////////////////////
 
 
 const firstUser = new User({
-    username: "Johnny Gumshoe",
+    username: "Ellen Drew",
     age: 27,
-    location: "Atlanta, GA",
+    location: "Chicago",
     authors: "Michael Collins, Raymond Chandler",
     network: "twitter.com/Gumshoe"
 })
 
 const secondUser = new User({
-    username: "Sofie Panch",
+    username: "John Harmon",
     age: 35, 
     location: "Atlanta, GA",
     authors: "Sheridan LeFanu, Agatha Christie",
-    network: "twitter.com/SofiePanch"
+    network: "twitter.com/J_Harmon"
+})
+
+const thirdUser = new User({
+    username: "Don Evans",
+    age: 42, 
+    location: "New York, NY",
+    authors: "James Ellroy",
+    network: "linkedin.com/in/don-evans"
+})
+
+const fourthUser = new User({
+    username: "Debbie Alton ",
+    age: 24, 
+    location: "Los Angeles, CA",
+    authors: "James Ellroy, Dashiell Hammett",
+    network: "instagram.com/fuzzyCats"
 })
 
 ///////////////////////////////////////////////////////////////
@@ -82,7 +139,7 @@ const secondUser = new User({
 
 User.deleteMany()
     .then(() => {
-        return User.insertMany([firstUser, secondUser])
+        return User.insertMany([firstUser, secondUser, thirdUser, fourthUser])
     })
     .then(() => {
         console.log('Done Seeding!')
@@ -91,7 +148,7 @@ User.deleteMany()
 
 Author.deleteMany()
     .then(() => {
-        return Author.insertMany([firstAuthor, secondAuthor, thirdAuthor])
+        return Author.insertMany([firstAuthor, secondAuthor, thirdAuthor, fourthAuthor, fifthAuthor, sixthAuthor])
     })
     .then(() => {
         console.log('Done Seeding!')
